a8893a18b21a49b9034e5581df91147cda31a995
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-10-15 02:52:21 +0300

    feat(new page): add reviews, slider, modals, icons, minification

--- new/js/scripts.js
+++ new/js/scripts.js
@@ -1,3 +1,4 @@
+prepareModals();
 initBurgerClick();
 
 function initBurgerClick () {
@@ -7,3 +8,135 @@ function initBurgerClick () {
         $('.navigation__list').slideToggle();
     })
 }
+
+function prepareModals() {
+    const $body = $('body');
+    $('[data-fancybox="modal"]').each(function (index) {
+        const img = '<img src="'+$(this).find('.reviews__photo').attr('src')+'" class="modal-review__img" alt="Фото ученика">';
+        const name = '<h2 hidden>'+$(this).find('.reviews__name').text()+'</h2>';
+        const left = '<div class="modal-review__left">'+img+name+'</div>';
+
+        const caption = '<p class="modal-review__caption">'+$(this).find('.reviews__caption').text()+'</p>';
+        const text = '<p class="modal-review__text">'+$(this).find('.reviews__text').text()+'</p>';
+        const right = '<div class="modal-review__right">'+text+caption+'</div>';
+
+        const id = 'modal-review-'+(index+1);
+
+        $(this).attr('href', '#'+id);
+        $body.append('<div class="modal-review" id="'+id+'" hidden>'+left+right+'</div>')
+    });
+}
+
+function initSlider(slider, options) {
+    slider.after('<div class="slick-nav"></div>');
+    const slickOptions = {
+        infinite: false,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        dots: true,
+        appendDots: slider.next('.slick-nav'),
+        appendArrows: slider.next('.slick-nav'),
+        swipeToSlide: true,
+        mobileFirst: true
+    };
+
+    slider.slick($.extend({}, slickOptions, options));
+}
+
+$(function () {
+    initSlider($('.results-list'), {
+        infinite: true,
+        slidesToShow: 1,
+        lazyLoad: 'ondemand',
+        responsive: [
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4,
+                }
+            }
+        ]
+    });
+});
+$(function () {
+    initSlider($('.reasons__list'), {
+        infinite: true,
+        slidesToShow: 1,
+        lazyLoad: 'ondemand',
+        responsive: [
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4,
+                }
+            }
+        ]
+    });
+});
+$(function () {
+    initSlider($('.advantages__list'), {
+        infinite: true,
+        slidesToShow: 1,
+        lazyLoad: 'ondemand',
+        responsive: [
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 6,
+                }
+            }
+        ]
+    });
+});
+
+$(function () {
+    initSlider($('.reviews__list'), {
+        infinite: true,
+        slidesToShow: 1,
+        lazyLoad: 'ondemand',
+        responsive: [
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 4,
+                }
+            }
+        ]
+    });
+});
