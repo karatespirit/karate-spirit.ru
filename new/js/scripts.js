866a0e6b4876b9e718663c517b800e8a862a6935
Denis Lukashevich <denis.lukashevich@megafon.ru>
2019-10-16 02:00:44 +0300

    feat(new page): add phone mask and sending form script

--- new/js/scripts.js
+++ new/js/scripts.js
@@ -1,6 +1,45 @@
 prepareModals();
 initBurgerClick();
 initMapPicking();
+initFormSend();
+initPhoneMasks();
+
+function initFormSend() {
+    $('form').submit(function(e) {
+        e.preventDefault();
+        const form = $(this);
+        const btn = form.find('input[type="submit"]');
+
+        const result = '<div class="form__result"><h4>Спасибо!</h4><p>Ваша заявка отправлена.<br>Мы свяжемся с Вами в ближайшее время.</p></div>';
+
+        $.ajax({
+            url: form.attr('action'),
+            data: form.serialize(),
+            type: "POST",
+            dataType: "xml",
+            beforeSend: function(){
+                btn.attr('disabled');
+            },
+            statusCode: {
+                0: function (){
+                    form.html(result);
+                },
+                200: function (){
+                    form.html(result);
+                }
+            }
+        });
+    });
+}
+
+function initPhoneMasks() {
+    const inputs = document.querySelectorAll('input[type="tel"]');
+    inputs.forEach(function (input) {
+        IMask(input, {
+            mask: '+{7}(000) 000-00-00'
+        });
+    });
+}
 
 function initBurgerClick () {
     $('.navigation__burger').on('click', function () {
